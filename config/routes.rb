44a5f303434d699053a145a0e39cfbccb2fae9de
Romeo <ralonsoad@gmail.com>
2023-08-19 19:55:21 +0200

    modification delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,10 @@
 
 Rails.application.routes.draw do
-  get 'bookmarks/index'
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/update'
-  get 'movies/index'
-  get 'movies/new'
-  get 'movies/create'
-  get 'movies/update'
-  get 'lists/index'
-  get 'lists/new'
-  get 'lists/create'
-  get 'lists/update'
-
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "lists#index"
-  resources :lists do
-    resources :bookmarks
-  end
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
 
-  resources :movies
+  end
+  resources :bookmarks, only: :destroy
 end
