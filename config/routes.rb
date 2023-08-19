dcb89d3c826910fa4626810a2bf01d7f865beacc
Romeo <ralonsoad@gmail.com>
2023-08-19 16:57:29 +0200

    before production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/update'
+  get 'movies/index'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/update'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/create'
+  get 'lists/update'
+
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+  resources :lists do
+    resources :bookmarks
+  end
+
+  resources :movies
 end
